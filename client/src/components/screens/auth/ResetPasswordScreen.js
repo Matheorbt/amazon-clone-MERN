f868573c1bdd47d414c256b2215bb4a93b637873
Mathéo Robert <57138558+Matheorbt@users.noreply.github.com>
2021-11-07 20:41:02 +0100

    client side done build error

---
+++ client/src/components/screens/auth/ResetPasswordScreen.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import axios from "axios";
+import "./ResetPasswordScreen.css";
+
+const ResetPasswordScreen = ({ match }) => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const resetPasswordHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  };
+
+  if (password !== confirmPassword) {
+    setPassword("");
+    setConfirmPassword("");
+    setTimeout(() => {
+      setError("");
+    }, 5000);
+    return setError("Passwords don't match");
+  }
+
+  try {
+    const { data } = axios.put(
+      `/api/auth/passwordreset/${match.param.resetToken}`,
+      { password },
+      config
+    );
+    console.log(data);
+    setSuccess(data.data);
+  } catch (error) {
+    setError(error.response.data.error);
+    setTimeout(() => {
+      setError("");
+    }, 5000);
+  }
+
+  return (
+    <div className="resetpassword-screen">
+      <form
+        action=""
+        className="resetpassword-screen__form"
+        onSubmit={resetPasswordHandler}
+      >
+        <h3 className="forgotpassword-screen__title">Reset password</h3>
+        {error && <span className="error-message">{error}</span>}
+        {success && (
+          <span className="success-message">
+            {success} <Link to="/login">Login</Link>
+          </span>
+        )}
+        <div className="form-group">
+          <label htmlFor="email">New password:</label>
+          <input
+            type="password"
+            required
+            id="password"
+            placeholder="Enter new password"
+            autoComplete="true"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Confirm new password:</label>
+          <input
+            type="password"
+            required
+            id="confirmpassword"
+            placeholder="Confirm new password"
+            autoComplete="true"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Reset password
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ResetPasswordScreen;
